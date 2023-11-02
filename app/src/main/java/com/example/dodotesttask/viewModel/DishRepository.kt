d2f98779172dece1b90f9e7d7bde1f141bef9f4d
Valerii09 <valiera.trufanov.2001@gmail.com>
2023-11-03 01:46:51 +0700

    изменен их дизайн

--- app/src/main/java/com/example/dodotesttask/viewModel/DishRepository.kt
+++ app/src/main/java/com/example/dodotesttask/viewModel/DishRepository.kt
@@ -15,6 +15,23 @@ class DishRepository {
         val dishes = mutableListOf<Dish>()
         dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
         dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
+        dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
+        dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
+
+        dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
+        dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
+        dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
+        dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
+
+        dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
+        dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
+        dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
+        dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
+
+        dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
+        dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
+        dishes.add(Dish("Пицца", "Аппетитная пицца с разнообразными топпингами", 9.99, R.drawable.pizza_image))
+        dishes.add(Dish("Салат Цезарь", "Свежий салат с курицей и соусом Цезарь", 7.49, R.drawable.pizza_image))
         // Добавьте другие блюда по аналогии
 
         dishesLiveData.value = dishes
